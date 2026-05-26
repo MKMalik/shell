ea71ad224f78b2fcfd231a0cfce45d6aa2b7670a
M.K. Malik <hello@mkmalik.com>
2026-05-26 13:05:08 +0530

    codecrafters submit [skip ci]

--- app/handlers/cd.go
+++ app/handlers/cd.go
@@ -1,7 +1,24 @@
 package handlers
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"os/user"
+)
 
 func HandleCd(dir string) {
+	if dir == "~" {
+		usr, err := user.Current()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = os.Chdir(usr.HomeDir)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		return
+	}
 	os.Chdir(dir)
 }
