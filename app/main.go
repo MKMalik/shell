e2d615c75121bc36563ecd88d52454915cea13cd
M.K. Malik <hello@mkmalik.com>
2026-05-26 10:08:16 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,10 +7,33 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
+var builtinNames = map[Builtin]struct{}{
+	// Echo: {},
+	// Type: {},
+}
+
+var builtins = map[Builtin]func(string){
+	Echo: handleEcho,
+	Type: handleType,
+}
+
+func isBuiltin(cmd string) bool {
+	_, ok := builtinNames[Builtin(cmd)]
+	return ok
+}
+
+type Builtin string
+
+const (
+	Exit Builtin = "exit"
+	Echo Builtin = "echo"
+	Type Builtin = "type"
+)
 
 func main() {
+	for i := range builtins {
+		builtinNames[i] = struct{}{}
+	}
 	scanner := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("$ ")
@@ -28,13 +51,28 @@ func main() {
 
 func processCmd(cmd string) {
 	switch strings.Split(cmd, " ")[0] {
-	case "echo":
-		handleEcho(cmd)
+	case string(Echo):
+		builtins[Echo](cmd)
+	case string(Type):
+		builtins[Type](cmd)
 	default:
 		fmt.Println(cmd + ": command not found")
 	}
 }
 
+func handleType(cmd string) {
+	fields := strings.Fields(cmd)
+	if len(fields) < 2 {
+		return
+	}
+	arg := fields[1]
+	if !isBuiltin(arg) {
+		fmt.Println(arg + ": not found")
+		return
+	}
+
+	fmt.Println(arg + " is a shell builtin")
+}
 func handleEcho(cmd string) {
 	args := strings.Fields(cmd)[1:]
 	fmt.Println(strings.Join(args, " "))
