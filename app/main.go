21206ec4f05004fb384c0d8fe195e8763fcb9ed0
M.K. Malik <hello@mkmalik.com>
2026-05-27 15:42:04 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -20,28 +20,51 @@ func main() {
 	scanner := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("$ ")
+
 		cmd, err := scanner.ReadString('\n')
 		if err != nil {
 			panic(err)
 		}
+
 		cmd = strings.TrimSpace(cmd)
+
 		if cmd == "exit" {
 			break
 		}
-		// check if cmd contains redirect stdout then redirect without printing
+
 		redirectTo := strings.Split(cmd, "1>")
+
 		if len(redirectTo) < 2 {
 			redirectTo = strings.Split(cmd, ">")
 		}
+
 		if len(redirectTo) > 1 {
 			redirectCmd := redirectTo[0]
 			redirect := redirectTo[1]
-			output := processCmd(redirectCmd)
-			handleRedirectToFile(strings.TrimSpace(output), strings.TrimSpace(redirect))
+
+			stdout, stderr := processCmd(redirectCmd)
+
+			handleRedirectToFile(
+				strings.TrimSpace(stdout),
+				strings.TrimSpace(redirect),
+			)
+
+			if stderr != "" {
+				fmt.Println(stderr)
+			}
+
 			continue
 		}
-		output := processCmd(cmd)
-		fmt.Println(output)
+
+		stdout, stderr := processCmd(cmd)
+
+		if stdout != "" {
+			fmt.Println(stdout)
+		}
+
+		if stderr != "" {
+			fmt.Println(stderr)
+		}
 	}
 }
 
@@ -54,35 +77,40 @@ func handleRedirectToFile(output, redirect string) {
 	file.WriteString(output)
 }
 
-func processCmd(command string) string {
+func processCmd(command string) (string, string) {
 	fields, _ := shlex.Split(command)
+
 	if len(fields) == 0 {
-		return ""
+		return "", ""
 	}
+
 	args := fields[1:]
+
 	switch fields[0] {
 	case string(handlers.Echo):
-		return handlers.Builtins[handlers.Echo](command)
+		return handlers.Builtins[handlers.Echo](command), ""
+
 	case string(handlers.Type):
-		return handlers.Builtins[handlers.Type](command)
+		return handlers.Builtins[handlers.Type](command), ""
+
 	case string(handlers.Pwd):
-		return handlers.Builtins[handlers.Pwd](command)
+		return handlers.Builtins[handlers.Pwd](command), ""
+
 	case string(handlers.Cd):
-		return handlers.Builtins[handlers.Cd](args[0])
+		return handlers.Builtins[handlers.Cd](args[0]), ""
+
 	default:
-		// check if exists in PATH as executable
 		found, _ := utils.ScanPath(os.Getenv("PATH"), fields[0])
-		// if exists and executable then execute passing args if any
+
 		if found != nil {
-			output, _ := runCommand(*found, args)
-			return output
+			stdout, stderr, _ := runCommand(*found, args)
+			return stdout, stderr
 		}
-		// if not: print command not found
-		return command + ": command not found"
+
+		return "", command + ": command not found"
 	}
 }
-
-func runCommand(found string, args []string) (string, error) {
+func runCommand(found string, args []string) (string, string, error) {
 	run := exec.Command(found, args...)
 
 	var stdout bytes.Buffer
@@ -93,10 +121,7 @@ func runCommand(found string, args []string) (string, error) {
 
 	err := run.Run()
 
-	output := strings.TrimRight(
-		stdout.String()+stderr.String(),
-		"\n",
-	)
-
-	return output, err
+	return strings.TrimRight(stdout.String(), "\n"),
+		strings.TrimRight(stderr.String(), "\n"),
+		err
 }
