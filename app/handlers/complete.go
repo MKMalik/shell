73282cfc3cca351b45cb451d604538cfa5d1c863
M.K. Malik <hello@mkmalik.com>
2026-05-31 23:11:58 +0530

    codecrafters submit [skip ci]

--- app/handlers/complete.go
+++ app/handlers/complete.go
@@ -3,13 +3,38 @@ package handlers
 import (
 	"fmt"
 	"strings"
+
+	"github.com/google/shlex"
 )
 
+var register = map[string]string{}
+
 func HandleComplete(cmd string) string {
-	split := strings.Split(cmd, "-p")
-	if len(split) < 2 {
+	fields, _ := shlex.Split(cmd)
+	if len(fields) == 0 {
 		return ""
 	}
-	arg := strings.TrimSpace(split[1])
-	return fmt.Sprintf("complete: %v: no completion specification", arg)
+
+	// complete -C <script> <cmd>
+	if len(fields) >= 3 && fields[1] == "-C" {
+		script := strings.Trim(fields[2], "'")
+		command := fields[3]
+
+		register[command] = script
+		return ""
+	}
+
+	// complete -p <cmd>
+	if len(fields) == 3 && fields[1] == "-p" {
+		command := fields[2]
+
+		script, ok := register[command]
+		if !ok {
+			return fmt.Sprintf("complete: %s: no completion specification\n", command)
+		}
+
+		return fmt.Sprintf("complete -C '%s' %s\n", script, command)
+	}
+
+	return ""
 }
