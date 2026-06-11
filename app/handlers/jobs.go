d8ee867181e91d71bc45bd01684273c22590561b
M.K. Malik <hello@mkmalik.com>
2026-06-11 19:17:34 +0530

    codecrafters submit [skip ci]

--- app/handlers/jobs.go
+++ app/handlers/jobs.go
@@ -1,18 +1,44 @@
 package handlers
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func HandleJobs(_ string) string {
+	var running []Job
+
 	for _, job := range JobList {
 		if job.Status == "Running" {
-			return fmt.Sprintf(
-				"[%d]+  %-24s%s\n",
-				job.ID,
-				job.Status,
-				job.CommandString,
-			)
+			running = append(running, job)
 		}
 	}
 
-	return ""
+	if len(running) == 0 {
+		return ""
+	}
+
+	var b strings.Builder
+
+	for i, job := range running {
+		marker := " "
+
+		switch {
+		case i == len(running)-1:
+			marker = "+"
+		case i == len(running)-2:
+			marker = "-"
+		}
+
+		fmt.Fprintf(
+			&b,
+			"[%d]%s  %-24s%s\n",
+			job.ID,
+			marker,
+			job.Status,
+			job.CommandString,
+		)
+	}
+
+	return b.String()
 }
