7e1ab4fc9d1a5f5343cc1784a6de0fd87a50c366
M.K. Malik <hello@mkmalik.com>
2026-06-18 19:25:35 +0530

    codecrafters submit [skip ci]

--- app/handlers/declare.go
+++ app/handlers/declare.go
@@ -5,9 +5,24 @@ import (
 	"strings"
 )
 
+var declareValues map[string]string = map[string]string{}
+
 func HandleDeclare(cmd string) string {
 	fields := strings.Fields(cmd)
-	// declareType := fields[1]
-	declareVar := fields[2]
-	return fmt.Sprintf("declare: %v: not found", declareVar)
+	declared := fields[1]
+	if declared == "-p" {
+		declareVar := fields[2]
+		if _, ok := declareValues[declareVar]; ok {
+			return fmt.Sprintf("declare -- %v=\"%v\"", declareVar, declareValues[declareVar])
+		}
+		return fmt.Sprintf("declare: %v: not found", declareVar)
+	} else if strings.Contains(declared, "=") {
+		splits := strings.Split(declared, "=")
+		if len(splits) < 2 {
+			return ""
+		}
+		key, val := splits[0], splits[1]
+		declareValues[key] = val
+	}
+	return ""
 }
