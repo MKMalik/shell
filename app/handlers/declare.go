567f37449b89b2870b3e02ee0bc45cd9ee054f60
M.K. Malik <hello@mkmalik.com>
2026-06-18 18:41:50 +0530

    codecrafters submit [skip ci]

--- app/handlers/declare.go
+++ app/handlers/declare.go
@@ -1,5 +1,13 @@
 package handlers
 
+import (
+	"fmt"
+	"strings"
+)
+
 func HandleDeclare(cmd string) string {
-	return ""
+	fields := strings.Fields(cmd)
+	// declareType := fields[1]
+	declareVar := fields[2]
+	return fmt.Sprintf("declare: %v: not found", declareVar)
 }
